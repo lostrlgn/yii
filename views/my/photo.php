db8d0a11cd075bea19f8350dbd0b93b5e8de22a5
Khanmirzaeva Louisa <frwithnum@gmail.com>
2024-10-23 18:51:37 +0300

    add slogan, change slider and add styles for photo

--- views/my/photo.php
+++ views/my/photo.php
@@ -3,16 +3,16 @@
     $path = '/img/';
 ?>
 
-<h1>Япония</h1>
-<div class="mt-5 d-flex flex-row justify-content-between">
+<h1 class="text-white bg-dark opacity-25 p-3 text-center">Откройте для себя очарование Японии</h1>
+<div  class="shadow p-3 mb-5 bg-white rounded mt-5 d-flex flex-row justify-content-evenly">
     <h3><?= array_keys($data)[0]?></h3>
     <img class="w-50" src="<?= $path . $data['Девушка в традиционной одежде'] ?>" alt="ddf">
 </div>
-<div class="mt-5 d-flex flex-row  justify-content-between">
+<div  class="shadow p-3 mb-5 bg-white rounded mt-5 d-flex flex-row justify-content-evenly">
     <img class="w-50" src="<?= $path . $data['Цветение сакуры'] ?>" alt="ddf">
     <h3><?= array_keys($data)[1]?></h3>
 </div>
-<div class="mt-5 d-flex flex-row justify-content-between">
+<div  class="shadow p-3 mb-5 bg-white rounded mt-5 d-flex flex-row justify-content-evenly">
     <h3><?= array_keys($data)[2]?></h3>
     <img class="w-50" src="<?= $path . $data['Японское искусство'] ?>" alt="ddf">
 </div>
--- views/slider/carousel.php
+++ views/slider/carousel.php
@@ -4,21 +4,22 @@ use yii\bootstrap5\Carousel;
 
     $path = '/img/';
     
-    
+echo'<h1 class="text-white bg-dark opacity-25 p-3 text-center">Откройте для себя очарование Японии</h1>';
+
 echo Carousel::widget([
       'items' => [
           [
-              'content' => '<img src="' . $path . $data[0] . '"/>',
+              'content' => '<img class="w-100" src="' . $path . $data[0] . '"/>',
               'caption' => '<h4>Япония</h4><p>Изображение девушки в традиционной одежде</p>',
               'captionOptions' => ['class' => ['d-none', 'd-md-block']],
           ],
           [
-              'content' => '<img src="' . $path . $data[1] . '"/>',
+              'content' => '<img class="w-100" src="' . $path . $data[1] . '"/>',
               'caption' => '<h4>Япония</h4><p>Изображение сакуры</p>',
               'captionOptions' => ['class' => ['d-none', 'd-md-block']],
           ],
           [
-              'content' => '<img src="' . $path . $data[2] . '"/>',
+              'content' => '<img class="w-100" src="' . $path . $data[2] . '"/>',
               'caption' => '<h4>Япония</h4><p>ИСкусство</p>',
               'captionOptions' => ['class' => ['d-none', 'd-md-block']],
           ],
